7220b6d8c0bd00ca7607711802031b6673cbb71d
Jaap-Jan de Wit <jipjan@gmail.com>
2016-10-23 22:27:57 +0200

    Commit with all code converted from our private repo to ASP.NET MVC Core

---
+++ src/LittleBitHome/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LittleBitHome.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Hackathon()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
