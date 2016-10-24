8177b4993c236877106881c7f279bfd52ea09d34
Jaap-Jan de Wit <jipjan@gmail.com>
2016-10-24 14:31:04 +0200

    gulp changes to run on build and clean

--- src/LittleBitHome/gulpfile.js
+++ src/LittleBitHome/gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding Clean='main-bower-files' />
+/// <binding BeforeBuild='Copy-font-files, BowerFiles-Copy-Clean' Clean='BowerFiles-Copy-Clean, Copy-font-files' />
 /*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
@@ -38,6 +38,6 @@ gulp.task("BowerFiles-Copy-Clean", function () {
 });
 
 gulp.task("Copy-font-files", function() {
-    return gulp.src("./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}")
+    return gulp.src("./bower_components/font-awesome/fonts/**/*.*")
         .pipe(gulp.dest("./wwwroot/lib/font-awesome/fonts"));
 })
